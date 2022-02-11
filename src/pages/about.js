e0f9270c1ddc282fc522e8946bedf13788912c20
Hiago Da Silva <hiagodasilvaconceicao@gmail.com>
2022-02-11 20:44:14 -0300

    Blog em manutenção

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
-import * as S from "./about-styled"
+import * as S from "../Styles/about-styled"
 
 const AboutPage = () => (
   <Layout>
