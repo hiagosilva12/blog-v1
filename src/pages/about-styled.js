63e31b9666987ac5d4c6bbe3f57d33161b7851a3
Hiago Da Silva <hiagodasilvaconceicao@gmail.com>
2022-02-11 18:49:23 -0300

    Blog em desenvolvimento

---
+++ src/pages/about-styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const AboutWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  color: var(--postColor);
+`
